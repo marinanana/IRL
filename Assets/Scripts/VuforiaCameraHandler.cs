450da112d8f62fa73151b9a68f9c63b5608771a0
Marina <marinamartinreguera@gmail.com>
2018-05-19 19:03:52 +0200

    Add menu and script for ar camera (disable camera on start)

---
+++ Assets/Scripts/VuforiaCameraHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IRL
+{
+    public class VuforiaCameraHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private bool disableCameraOnStart;
+
+        void Start()
+        {
+            if (disableCameraOnStart)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
