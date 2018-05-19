1e3a42fb2631a7b6177981b39024bfc245ab6472
Marina <marinamartinreguera@gmail.com>
2018-05-19 19:42:06 +0200

    Create scan button prefab and its script

---
+++ Assets/Scripts/ScanButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IRL
+{
+    public class ScanButton : MonoBehaviour
+    {
+        [SerializeField]
+        private string number;
+        [SerializeField]
+        private string theme;
+
+        [SerializeField]
+        private Text numberText;
+        [SerializeField]
+        private Text themeText;
+
+        void Start()
+        {
+            numberText.text = number;
+            themeText.text = theme;
+        }
+    }
+}
